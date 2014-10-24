5b15a72b046e543b2f4c92263e7f7f69ee4dfb07
VivekBharatha <vivek.vivex@gmail.com>
2014-10-24 17:30:22 +0530

    Initial

---
+++ core.php
@@ -0,0 +1,91 @@
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6 col-sm-offset-3">
+
+
+<?php
+	echo '<div class="well">';
+	if(isset($_FILES['file'])) {
+    	if($_FILES['file']['type']!='text/xml'){
+    		echo "<h3>Please upload xml file only !</h3>";
+    	} else {
+	    	$dir = 'uploads/';
+	    	$fileName = explode('.',$_FILES['file']['name']);
+	    	$fileName = uniqid(time(),true) . "." . end($fileName);
+	    	$target = $dir . $fileName;
+	    	if(move_uploaded_file($_FILES['file']['tmp_name'], $target)) {
+	    		echo "<h3>The file ".$_FILES['file']['name']." has been uploaded</h3>";
+	    		echo "<h4>Please click generate button!</h4>";
+	    	} else {
+	    		echo "<h3>There was some problem in uploading file.Pleas try again later</h3>";
+	    	}
+    	}
+    }
+    if(isset($target)) {
+
+
+	$file = $target;
+	if(file_exists($file)) {
+		$xml = simplexml_load_file($target);
+		$formGen = process($xml);
+		echo "<form style='margin-left:40%' method='post' action='generated.php'> <input type='hidden' name='form' value=\"$formGen\" />";
+		echo "<input class='btn btn-success' type='submit' value='Generate' />";
+		echo "</form>";
+	}
+	else
+		echo 'File couldn\'t be loaded';
+	}
+
+echo "</div>";
+	function process($xml) {
+		$form = form_init();
+			foreach ($xml as $field) {
+				if($field->getName()=="select"){ //<-------- Select Tag Start
+					$form .=  "<select name='".$field->name ."' class='form-control ".$field->class."'>\n";
+					foreach($field->options as $options) {
+						foreach ($options as $option) {
+							$form .= "<option value='".$option->value."'>".$option->getName()."</option>\n";
+						}
+					}
+
+					$form .= "</select></br>\n"; //<--------Select Tag End
+				}
+				elseif($field->getName()=="textarea"){
+					$form .=  "<textarea name='".$field->name ."' placeholder='".$field->placeholder."' class='form-control ".$field->class."'></textarea></br>\n";
+				}
+				elseif($field->getName()=="radio"){
+					$form .= "<label class='radio-inline'>\n";
+					$form .= "<input type='".$field->getName()."' name='".$field->name ."' value='".$field->value."' class='".$field->class."' />\n";
+					$form .= $field->text."\n</label>\n";
+				}
+				elseif($field->getName()=="checkbox"){
+					$form .= "<label class='checkbox-inline'>\n";
+					$form .= "<input type='".$field->getName()."' name='".$field->name ."' value='".$field->value."' class='".$field->class."' />\n";
+					$form .= $field->text."\n</label><br>\n";
+				}
+				elseif($field->getName()=="submit") {
+						$form .=  "<input type='".$field->getName()."' name='".$field->name ."' placeholder='".$field->placeholder."' class='".$field->class."' /></br>\n";
+				}
+				else {
+						$form .=  "<input type='".$field->getName()."' name='".$field->name ."' placeholder='".$field->placeholder."' class='form-control ".$field->class."' /></br>\n";
+				}
+			}
+
+		$form = form_close($form);
+		return $form;
+	}
+
+	function form_init() {
+		$form = "<form role='form' method='post' action=''>\n";
+		return $form;
+	}
+
+	function form_close($form) {
+		$form .= "</form>\n";
+		return $form;
+	}
+?>
+		</div>
+	</div>
+</div>
---
+++ generated.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FormGen</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="gcp/prettify.css" type="text/css" rel="stylesheet" />
+    <link href="css/obs.css" type="text/css" rel="stylesheet" />
+	  <script type="text/javascript" src="gcp/prettify.js"></script>
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <a class="btn btn-danger" href="index.php">Index</a>
+    <div class="col-sm-6">
+    	<div class="well">
+    	<?php echo $_POST['form'];?>
+    	</div>
+    </div>
+    <div class="col-sm-12">
+    <div class="well">
+    <pre class="prettyprint"> <?php echo htmlspecialchars($_POST['form']);?></pre>
+    </div>
+    </div>
+
+
+
+
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+
+(function(jQuery){
+
+  jQuery( document ).ready( function() {
+
+    prettyPrint();
+
+  } );
+
+}(jQuery))
+
+</script>
+  </body>
+</html>
