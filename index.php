23ba2cf702cbd410e8b085a40b74dff888b23d0f
VivekBharatha <vivek.vivex@gmail.com>
2014-10-24 17:56:43 +0530

    Update Readme
    And some code formatting

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html>
-<head>
-<title>Upload file</title>
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-<div class="container">
-
-    <div class="row" style="margin-top: 100px;">
-	    <div class="col-sm-6 col-sm-offset-3 well">
-	    <h1>Upload the xml file</h1>
-	    	<form class="form" role="form" action="core.php" method="post" enctype="multipart/form-data">
-				<input style="float: left;" type="file" name="file" />
-				<input  style="float: left;" class="btn btn-info" type="submit" value="Upload File" />
-			</form>
-	    </div>
-    </div>
-   </div>
-    </body>
-    </html>
+	<head>
+		<title>Upload file</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+		    <div class="row" style="margin-top: 100px;">
+			    <div class="col-sm-6 col-sm-offset-3 well">
+			    <h1>Upload the xml file</h1>
+			    	<form class="form" role="form" action="core.php" method="post" enctype="multipart/form-data">
+						<input style="float: left;" type="file" name="file" />
+						<input  style="float: left;" class="btn btn-info" type="submit" value="Upload File" />
+					</form>
+			    </div>
+		    </div>
+		</div>
+	</body>
+</html>
